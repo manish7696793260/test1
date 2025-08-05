55a8f9fb5a24447068a6ae8fe9da4f9e0523a259
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-08-05 18:08:48 +0530

    Update be-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-be-pipeline/be-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-be-pipeline/be-pipeline-deploy.sh
@@ -12,7 +12,7 @@ SAMOutputFile=${SAMOUTPUT}
 
 stackname=$env-$pipeline_project-pipeline
 bucket_name=myawsbucket-template
-eu_bucket_name=manish-dabkhera
+eu_bucket_name=manish-dabkhera2
 euprd_bucket_name=euprd-common-templates
 usprd_bucket_name=usprd-common-templates
 
