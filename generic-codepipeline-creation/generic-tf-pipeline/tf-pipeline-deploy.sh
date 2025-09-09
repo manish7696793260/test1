f680571072589046e2f9f9b539f467d284bbd843
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-09-09 12:38:37 +0530

    Update tf-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
@@ -5,4 +5,4 @@ mv terraform /usr/local/bin/
 unzip terraform10.zip
 cd terraform10
 terraform init
-terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE"
+terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE" --auto-approve
