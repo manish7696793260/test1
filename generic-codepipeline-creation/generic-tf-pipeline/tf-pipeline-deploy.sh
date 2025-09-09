0372fc5f1a7c0e69a40c066903b98579e5f14a4b
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-09-09 12:30:51 +0530

    Update tf-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
@@ -2,7 +2,7 @@
  wget https://releases.hashicorp.com/terraform/1.11.2/terraform_1.11.2_linux_amd64.zip
 unzip terraform_1.11.2_linux_amd64.zip
 mv terraform /usr/local/bin/
-unzip codepipeline.zip
-cd codepipeline
+unzip terraform10.zip
+cd terraform10
 terraform init
 terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE"
