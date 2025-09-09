390d8b5d5f07b1e2ba700c3c4069b1710a22a2eb
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-09-09 12:24:37 +0530

    Update tf-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo dnf install -y terraform
+ wget https://releases.hashicorp.com/terraform/1.11.2/terraform_1.11.2_linux_amd64.zip
+unzip terraform_1.11.2_linux_amd64.zip
+mv terraform /usr/local/bin/
 terraform init
 terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE"
