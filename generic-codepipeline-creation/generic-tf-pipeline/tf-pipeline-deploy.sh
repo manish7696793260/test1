d800fee3ad4cfc8c13e9e127fc84d13bce08faf0
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-09-09 14:23:12 +0530

    Update tf-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
@@ -6,5 +6,5 @@ mv terraform /usr/local/bin/
 unzip terraform10.zip
 cd terraform10 
 
-terraform init -backend-config="bucket=bucketforerraform" - backend-config="key=${PIPELINE_NAME}/terraform.tfstate" -backend-config="region=us-west-2"
+terraform init -backend-config="bucket=bucketforerraform" -backend-config="key=usw2-common-tf/terraform.tfstate" -backend-config="region=us-west-2"
 terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE" --auto-approve
