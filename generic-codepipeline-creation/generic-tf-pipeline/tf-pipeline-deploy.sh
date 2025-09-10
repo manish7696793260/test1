13e8b58c98e11b6b9318e6dd578d7858c28d1a6c
manish7696793260 <36193082+manish7696793260@users.noreply.github.com>
2025-09-10 12:33:24 +0530

    Update tf-pipeline-deploy.sh

--- generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
+++ generic-codepipeline-creation/generic-tf-pipeline/tf-pipeline-deploy.sh
@@ -5,6 +5,8 @@ unzip terraform_1.11.2_linux_amd64.zip
 mv terraform /usr/local/bin/
 unzip terraform10.zip
 cd terraform10 
+aws codepipeline list-pipelines --query 'pipelines[].name' --output text
 
-terraform init -backend-config="bucket=bucketforerraform" -backend-config="key=usw2-common-tf/terraform.tfstate" -backend-config="region=us-west-2"
-terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE" --auto-approve
+
+#terraform init -backend-config="bucket=bucketforerraform" -backend-config="key=usw2-common-tf/terraform.tfstate" -backend-config="region=us-west-2"
+#terraform apply -var="pipeline_region=$PIPELINE_REGION" -var="environment_variable=$ENVIRONMENT_VARIABLE" -var="pipeline_name=$PIPELINE_NAME" -var="codestar_connection_name=$CODESTAR_CONNECTION_NAME" -var="region=$REGION" -var="deploymentregion=$DEPLOYMENTREGION" -var="tf_var=$TF_VAR" -var="countryenv=$COUNTRYENV" -var="state_file_path=$STATE_FILE_PATH" -var="state_file_bucket=usnp-usw2-terraform-statefile-bucket" -var="db_table=$DB_TABLE" --auto-approve
